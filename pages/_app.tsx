87c60eefe81644bbf7f9c0b3fee60f552cde0d70
Marco Pfeiffer <marco@hauptsache.net>
2022-06-19 21:07:59 +0200

    implement tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { trackPageView } from "../src/tracker";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps, router }: AppProps) {
+  const canonicalUrl = `${process.env.NEXT_PUBLIC_URL}${router.pathname}`;
+  useEffect(() => {
+    trackPageView(canonicalUrl);
+  }, [canonicalUrl]);
+
   return <Component {...pageProps} />;
 }
 export default MyApp;
